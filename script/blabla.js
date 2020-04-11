7570cf396106ad8c9ffcc866ca37be623e52db62
Patrick van Bergen <patrick.vanbergen@gmail.com>
2020-04-11 21:29:58 +0200

    processes

--- script/blabla.js
+++ script/blabla.js
@@ -1,1 +1,60 @@
-BlaBla = {};
+BlaBla = {
+
+    processes: [],
+
+    addProcess: function (component, element) {
+
+        for (let a = 0; a < component.animations.length; a++) {
+            component.animations[a].setup();
+        }
+
+        let process = {
+            time: 0,
+            component: component,
+            element: element,
+            state: "paused",
+            start: function () {
+                this.state = "running";
+            },
+            pause: function () {
+                this.state = "paused";
+            },
+            continue: function () {
+                this.state = "running";
+            },
+            toggleState: function () {
+                if (this.state === "paused") {
+                    this.state = "running";
+                } else {
+                    this.state = "paused";
+                }
+            }
+        };
+
+        this.processes.push(process);
+
+        return process;
+    },
+
+    start: function () {
+        this.step();
+    },
+
+    step: function () {
+
+        for (let i = 0; i < this.processes.length; i++) {
+            let process = this.processes[i];
+            if (process.state === "paused") {
+                continue;
+            }
+            let animations = process.component.animations;
+            for (let a = 0; a < animations.length; a++) {
+                animation = animations[a];
+                animation.step(process.time);
+            }
+            process.time++;
+        }
+
+        window.requestAnimationFrame(() => { this.step(); });
+    }
+};
--- script/type/starwars.js
+++ script/type/starwars.js
@@ -1,72 +1,63 @@
 BlaBla.starwars = {
     // https://www.youtube.com/watch?v=yHfLyMAHrQE
     // http://www.theforce.net/fanfilms/postproduction/crawl/opening.asp
-    createAnimation: function (element, data) {
+    createComponent: function (element, data) {
+
         return {
+            animations: [
+                {
+                    text: null,
 
-            pos: 400,
-            text: null,
-            state: "stopped",
+                    setup: function () {
+                        element.style.perspective = "200px";
+                        element.style.overflow = "hidden";
+                        element.style.backgroundColor = "#000";
 
-            start: function () {
+                        let surface = BlaBla.base.createDiv();
+                        surface.style.width = "470px";
+                        surface.style.height = "430px";
+                        surface.style.color = '#ffffff';
+                        surface.style.marginLeft = 'auto';
+                        surface.style.marginRight = 'auto';
+                        surface.style.transform = 'rotateX(40deg)';
+                        surface.style.overflow = "hidden";
+                        element.appendChild(surface);
 
-                element.style.perspective = "200px";
-                element.style.overflow = "hidden";
-                element.style.backgroundColor = "#000";
+                        let text = BlaBla.base.createDiv();
+                        text.style.transform = "translateY(" + this.pos + "px)";
+                        text.style.width = "100%";
+                        text.style.color = "#c8a90b";
+                        text.style.fontFamily = "FranklinGothicBook";
+                        text.style.fontSize = "30pt";
+                        text.style.fontWeight = "bold";
+                        text.style.textAlign = "justify";
+                        surface.appendChild(text);
 
-                let surface = BlaBla.base.createDiv();
-                surface.style.width = "470px";
-                surface.style.height = "430px";
-                surface.style.color = '#ffffff';
-                surface.style.marginLeft = 'auto';
-                surface.style.marginRight = 'auto';
-                surface.style.transform = 'rotateX(40deg)';
-                surface.style.overflow = "hidden";
-                element.appendChild(surface);
+                        let overlay = BlaBla.base.createDiv();
+                        overlay.style.top = "0";
+                        overlay.style.position = "absolute";
+                        overlay.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 1.0), rgba(255, 0, 0, 0.0))";
+                        overlay.style.width = "100%";
+                        overlay.style.height = "100%";
+                        surface.appendChild(overlay);
 
-                let text = BlaBla.base.createDiv();
-                text.style.transform = "translateY(" + this.pos + "px)";
-                text.style.width = "100%";
-                text.style.color = "#c8a90b";
-                text.style.fontFamily = "FranklinGothicBook";
-                text.style.fontSize = "30pt";
-                text.style.fontWeight = "bold";
-                text.style.textAlign = "justify";
-                surface.appendChild(text);
+                        this.text = text;
+                        text.innerHTML = data.text;
+                    },
+                    step: function (time) {
+                        if (time < 1450) {
+                            this.text.style.transform = "translateY(" + (400 - time) + "px)";
+                        }
+                    }
+                }, {
+                    setup: function () {
 
-                let overlay = BlaBla.base.createDiv();
-                overlay.style.top = "0";
-                overlay.style.position = "absolute";
-                overlay.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 1.0), rgba(255, 0, 0, 0.0))";
-                overlay.style.width = "100%";
-                overlay.style.height = "100%";
-                surface.appendChild(overlay);
+                    },
+                    step: function (time) {
 
-                this.text = text;
-                text.innerHTML = data.text;
-
-                this.state = "running";
-                this.step()
-
-            },
-            pause: function () {
-                this.state = "paused";
-            },
-            continue: function () {
-                this.state = "running";
-            },
-            step: function() {
-                if (this.pos < -1000) {
-                    return;
+                    }
                 }
-                if (state === "running") {
-                    this.pos = this.pos - 1;
-                    this.text.style.transform = "translateY(" + this.pos + "px)";
-                }
-                window.setTimeout(() => {
-                    this.step();
-                }, 50)
-            }
+            ]
         }
     }
 };
