a3a79ddfbf7bfc870aaed058aa6148cf009264da
Patrick van Bergen <patrick.vanbergen@gmail.com>
2020-05-09 19:26:32 +0200

    time animation

--- time/time.js
+++ time/time.js
@@ -3,38 +3,67 @@ BlaBla.time = {
     // https://www.youtube.com/watch?v=8EdxM72EZ94
     createComponent: function (element, data) {
 
+        const firstStart = 0;
+        const firstDuration = 13000;
+        const secondStart = 9000;
+        const secondDuration = 9000;
+        const timeRange = 18000;
+
         return {
             setup: function() {
                 element.style.backgroundColor = "#343336";
             },
             animations: [
                 {
-                    largeText: null,
-                    smallText: null,
+                    surfaces: [],
+                    largeTexts: [],
+                    smallTexts: [],
 
                     setup: function () {
 
-                        let smallText = document.createElement('div');
-                        smallText.className = "small-text";
-                        element.appendChild(smallText);
+                        let shadowClass;
 
-                        let largeText = document.createElement('div');
-                        largeText.className = "large-text";
-                        element.appendChild(largeText);
+                        for (let i = 0; i < 2; i++) {
 
-                        this.largeText = largeText;
-                        largeText.innerHTML = data.largeText;
-                        this.smallText = smallText;
-                        smallText.innerHTML = data.smallText;
+                            let surface = document.createElement('div');
+                            surface.className = "surface";
+                            surface.style.transformOrigin = "center";
+                            element.appendChild(surface);
+                            this.surfaces[i] = surface;
+
+                            let smallText = document.createElement('div');
+                            shadowClass = (i === 0) ? 'small-shadow-top' : 'small-shadow-left';
+                            smallText.className = "small-text " + shadowClass;
+                            smallText.innerHTML = data.smallText;
+                            surface.appendChild(smallText);
+                            this.smallTexts[i] = smallText;
+
+                            let largeText = document.createElement('div');
+                            shadowClass = (i === 0) ? 'large-shadow-top' : 'large-shadow-left';
+                            largeText.className = "large-text " + shadowClass;
+                            largeText.innerHTML = data.largeText;
+                            surface.appendChild(largeText);
+                            this.largeTexts[i] = largeText;
+                        }
+
+                        this.surfaces[1].style.transform = "rotateZ(-30deg)";
+                        this.largeTexts[1].style.transform = "translateX(200vw)";
+                        this.smallTexts[1].style.transform = "translateX(200vw)";
                     },
                     step: function (time) {
 
-                        time = time % 9000;
+                        time = time % timeRange;
 
-                        let start = 0;
-                        if (time >= start && time < start + 9000) {
-                            this.largeText.style.transform = "translateX(" + (50 - (time-start) / 50) + "%)";
-                            this.smallText.style.transform = "translateX(" + (50 - (time-start) / 50) + "%)";
+                        if (time >= firstStart && time < firstStart + firstDuration) {
+                            this.largeTexts[0].style.transform = "translateX(" + (100 - (time-firstStart) / 50) + "vw)";
+                            this.smallTexts[0].style.transform = "translateX(" + (100 - (time-firstStart) / 75) + "vw)";
+                        }
+
+                        if (time >= secondStart && time < secondStart + secondDuration) {
+                            this.largeTexts[1].style.transform =
+                                "translateX(" + (75 - (time-secondStart) / 50) + "vw) translateY(" + (-100 + (time-secondStart) / 75) + "vh)";
+                            this.smallTexts[1].style.transform =
+                                "translateX(" + (85 - (time-secondStart) / 75) + "vw) translateY(" + (-100 + (time-secondStart) / 50) + "vh)";
                         }
                     }
                 }
